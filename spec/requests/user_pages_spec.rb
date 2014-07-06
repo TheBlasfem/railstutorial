5275b9fe606884dc4ea0e3a6d3f007b2a40e3dba
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-06 11:23:03 -0500

    test for signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,27 @@ describe 'User Page' do
   	it { should have_content(user.name) }
   	#it { should have_content(user.email) }
   end
+
+  describe "sign up" do 
+    let(:submit) { "Create User" }
+    before { visit signup_path }
+    
+    describe "with invalid information" do
+      it "should not create user" do 
+        expect { click_link submit }.not_to change(User.count)
+      end
+    end
+    
+    describe "with valid information" do 
+      before do
+        fill_in "Name", with: "Julio"
+        fill_in "Email", with: "probando@gmail.com"
+        fill_in "Password", with: "12345678"
+        fill_in "Confirmation", with: "12345678"
+      end
+      it "should create user" do
+        expect { click_link submit }.to change(User.count).by(1)
+      end
+    end
+  end
 end
