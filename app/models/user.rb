30ba915c9f5f3b089e6c9f7f5a63d61360e61e08
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-06 14:09:56 -0500

    sign_in method, remember token to cookie and digest token to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i }
 	before_save { email.downcase! }
-	before_create { :create_remember_token }
+	before_create :create_remember_token
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
 	private
 	def create_remember_token
-		User.digest(User.new_remember_token)
+		self.remember_token = User.digest(User.new_remember_token)
 	end
 
 end
