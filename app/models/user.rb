e13170863f4d5be80fca01bad5cf7d478014bcf6
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-06 13:51:44 -0500

    create remember token by digest sha1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i }
 	before_save { email.downcase! }
+	before_create { :create_remember_token }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+
+	def self.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def self.digest(token)
+		Digest::SHA1::hexdigest(token.to_s)
+	end
+
+	private
+	def create_remember_token
+		User.digest(User.new_remember_token)
+	end
+
 end
