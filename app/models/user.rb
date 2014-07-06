3ec899ceeaeb5e3dc2c6565d3ef85b38cb78195c
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-05 22:51:48 -0500

    specs and validation from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+	before_save { self.email = email.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705173249) do
-
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140706033751) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -26,5 +19,7 @@ ActiveRecord::Schema.define(version: 20140705173249) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
