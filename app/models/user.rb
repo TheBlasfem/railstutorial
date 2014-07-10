ff8c9abd6cbc60d554d61c65ac9935cd24a18073
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-10 00:25:57 -0500

    feed items

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,13 @@ class User < ActiveRecord::Base
 		Digest::SHA1::hexdigest(token.to_s)
 	end
 
-	private
-	def create_remember_token
-		self.remember_token = User.digest(User.new_remember_token)
+	def feed
+		Micropost.where("user_id = ?", id)
 	end
+
+	private
+		def create_remember_token
+			self.remember_token = User.digest(User.new_remember_token)
+		end
 
 end
