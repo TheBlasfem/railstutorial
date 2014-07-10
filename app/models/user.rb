f8030ae0fd49d62d9ecbd6f2c86639076efc0e9b
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-10 09:42:28 -0500

    associations users and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i }
 	before_save { email.downcase! }
