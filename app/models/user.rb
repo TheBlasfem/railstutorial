eeee4836c545b4cea345a1b06bcb860520086160
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-10 10:37:17 -0500

    followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id", 
+																	 class_name: "Relationship",
+																	 dependent: :destroy
+  #it can be has_many :follower_users, through: :reverse_relationships, source: :follower
+	has_many :followers, through: :reverse_relationships
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i }
 	before_save { email.downcase! }
