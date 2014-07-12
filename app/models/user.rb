204ce89a4b7bccaf03baa612b483afbf2dd851fe
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-11 23:35:51 -0500

    view stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
 	has_many :reverse_relationships, foreign_key: "followed_id", 
-																	 class_name: "Relationship",
-																	 dependent: :destroy
+									 class_name: "Relationship",
+									 dependent: :destroy
   #it can be has_many :follower_users, through: :reverse_relationships, source: :follower
 	has_many :followers, through: :reverse_relationships
 	validates :name, presence: true, length: { maximum: 50 }
