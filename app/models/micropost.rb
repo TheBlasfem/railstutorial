47a4f792d80c46947a1982b62a1a967e0b940e4b
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-07-12 01:47:12 -0500

    feed show microposts from follwing users and me

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,14 @@ class Micropost < ActiveRecord::Base
 	validates :user_id, presence: true
 	validates :content, presence: true, length: { maximum: 140 }
 	default_scope -> { order("created_at DESC") }
+
+	def self.from_users_followed_by(user)
+    #followed_user_ids = user.followed_user_ids
+    #where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    #previous lines do the same, but the next lines do with better perfomance thanks to raw sql
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
 end
